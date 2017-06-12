d2f0e3366df60ad9805f0c50d448efcd66b94e81
Aluno <Aluno@172.5.1.162>
2017-06-12 19:58:34 -0300

    alteracao

--- src/testeGit/ClasseTeste.java
+++ src/testeGit/ClasseTeste.java
@@ -5,8 +5,8 @@ public class ClasseTeste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		
-
+		// TODO Auto-generated method stub
+	
 	}
 
 }
