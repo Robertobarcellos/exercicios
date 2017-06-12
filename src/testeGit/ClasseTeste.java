83fe5ac25f08fb6175a5c3361e3c47a2c8da01d3
Aluno <Aluno@172.5.1.162>
2017-06-12 20:01:58 -0300

    teste

--- src/testeGit/ClasseTeste.java
+++ src/testeGit/ClasseTeste.java
@@ -6,6 +6,8 @@ public class ClasseTeste {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		// TODO Auto-generated method stub
+	System.out.println("ola mundo");
+	
 	
 	}
 
